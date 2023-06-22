9b29c731f018c14c752aa3a25141646dd4ae6b13
Stefania <101118482+Stfelz6@users.noreply.github.com>
2023-06-23 00:56:10 +0300

    Verifying quality on production

--- src/App.tsx
+++ src/App.tsx
@@ -20,8 +20,60 @@ import {
 import { PageNotFound, Unlock } from 'pages';
 import { routeNames } from 'routes';
 import { routes } from 'routes';
+// import { DataStore } from '@aws-amplify/datastore';
+// import { Amplify, API, graphqlOperation } from 'aws-amplify';
+// import * as queries from './graphql/queries';
+// import * as mutations from './graphql/mutations';
+// import * as subscriptions from './graphql/subscriptions';
+// import schema from './models/schema'
+// import {
+// 	BrowserRouter as Router,
+// 	Routes,
+// 	Route,
+// 	Link,
+//   Navigate,
+//   useParams,
+//   useNavigate
+// } from 'react-router-dom';
+// import consoleLogo from './consoleLogo.png';
+// import AddQuestionJUSTTESTING from './Components/HUD/LoggedDashboard/AddQuestionJUSTTESTING';
+
+// Amplify.configure(awsconfig);
+// var AWS = require("aws-sdk");
 
 export const App = () => {
+  // const putNewUserToCustomDB = async (user) => {
+  //   console.log("User was put in DB.");
+  //   const userDetails = {
+  //     nickname: user.attributes['custom:nickname'],
+  //     role: user.attributes['custom:role'],
+  //     uICode: user.attributes['custom:uICode'],
+  //     email: user.attributes['email'],
+  //     sub:user.attributes['sub'],
+  //     currency:0,
+  //     grade:0,
+  //     subjectsLiked:"",
+  //     expectations:"",
+  //     totalHrsSpent:0,
+  //     totalGoodAns:0,
+  //     totalBadAns:0,
+  //     totalCheatTries:0,
+  //     avgRoGrade:0,
+  //     avgMatGrade:0
+  //   };
+  //   try {
+  //     const newUser = await API.graphql({
+  //       query: mutations.createUser,
+  //       variables: { input: userDetails }
+  //     });
+  //     console.log("User has been created:", newUser);
+  //   } catch (error) {
+  //     console.log("Error creating user:", error);
+  //   } finally {
+  //     createUserLockRef.current = false; // Release the lock
+  //   }
+  // }
+
   return (
     <AxiosInterceptorContext.Provider>
       <AxiosInterceptorContext.Interceptor
